b69e7e9f4af21270f983f65e5a8e69c122664f8c
Waleed Tariq <waleed.tariq@sukuktech.com>
2024-07-15 01:02:01 +0500

    variabkes

---
+++ 1.datatypes-variables/datatypes.php
@@ -0,0 +1,35 @@
+<?php
+
+// String
+$name = 'Waleed';
+var_dump($name);
+echo '<br/>';
+
+// Integers
+$age = 23;
+var_dump($age);
+echo '<br/>';
+
+// Float
+$rating = 8.7;
+var_dump($rating);
+echo '<br/>';
+
+// Boolean
+$isLoaded = true;
+var_dump($isLoaded);
+echo '<br/>';
+
+// Array
+$friends = ['Waleed', 'Saqi'];
+var_dump($friends);
+echo '<br/>';
+
+// Object
+$person = new stdClass();
+var_dump($person);
+echo '<br/>';
+
+// Null
+$car = null;
+var_dump($car);
---
+++ 1.datatypes-variables/variables.php
@@ -0,0 +1,31 @@
+<?php
+$title = 'Learn PHP From Scratch';
+$courseTitle = 'Welcome To The Course';
+$courseDesc = 'In this course, you will learn the fundamentals of the PHP language'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title><?= $title ?></title>
+</head>
+
+<body class="bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold"><?= $title ?></h1>
+        </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6">
+            <h2 class="text-2xl font-semibold mb-4"><?= $courseTitle ?></h2>
+            <p><?= $courseDesc ?></p>
+        </div>
+    </div>
+</body>
+
+</html>
