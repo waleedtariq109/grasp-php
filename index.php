11cf21504ba1615c1a0d332bf7dcaf62eaf4b1a8
Waleed Tariq <waleed.tariq@sukuktech.com>
2024-07-18 03:56:25 +0500

    arrow function

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php
+
+$add = fn ($a, $b) => $a + $b;
+echo $add(1, 2) . '<br>';
+
+$numbers = [1, 2, 3, 4, 5];
+$squaredNumber = array_map(fn ($val) => $val * $val, $numbers);
+print_r($squaredNumber);
