51266d01d6c52a8caf1d0f0b443ff5dcf3364709
Waleed Tariq <waleed.tariq@sukuktech.com>
2024-07-18 04:32:54 +0500

    opp: classes

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php
+
+class User
+{
+    public $name;
+    public $email;
+
+    public function __construct($name, $email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function login()
+    {
+        echo "$this->name is logged in";
+    }
+}
+
+$user1 = new User('Waleed Tariq', 'waleed@gmail.com');
+$user1->login();
+
+$user2 = new User('John Doe', 'john@gmail.com');
+$user2->login();
+
+// var_dump($user1);
+// var_dump($user2);
