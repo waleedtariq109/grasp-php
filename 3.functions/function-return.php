49ae37ddd9b8f713bf7eb8aafe6c4122da4168a4
Waleed Tariq <waleed.tariq@sukuktech.com>
2024-07-18 00:10:12 +0500

    function with returns

---
+++ 3.functions/function-return.php
@@ -0,0 +1,18 @@
+<?php
+
+function sayHello()
+{
+    echo "Hello Waleed";
+}
+
+// sayHello();
+
+// Function With return
+
+function sayGoodBye()
+{
+    return 'Good Bye';
+}
+
+$goodBye = sayGoodBye();
+echo $goodBye;
